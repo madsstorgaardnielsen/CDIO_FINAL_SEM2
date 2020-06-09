ab8d0e224e1f01eec970220cab0a2a1c288692ae
Mads Storgaard-Nielsen <madssn@hotmail.com>
2020-06-09 13:32:49 +0200

    Added input validation + tests

--- src/main/java/dto/ProductBatchDTO.java
+++ src/main/java/dto/ProductBatchDTO.java
@@ -1,4 +1,80 @@
 package dto;
 
 public class ProductBatchDTO {
+    int productBatchId;
+    int recipeId;
+    int status;
+    int userId;
+    int ingredientBatchId;
+    double tara;
+    double netto;
+
+    public int getProductBatchId() {
+        return productBatchId;
+    }
+
+    public void setProductBatchId(int productBatchId) {
+        this.productBatchId = productBatchId;
+    }
+
+    public int getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(int recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getIngredientBatchId() {
+        return ingredientBatchId;
+    }
+
+    public void setIngredientBatchId(int ingredientBatchId) {
+        this.ingredientBatchId = ingredientBatchId;
+    }
+
+    public double getTara() {
+        return tara;
+    }
+
+    public void setTara(double tara) {
+        this.tara = tara;
+    }
+
+    public double getNetto() {
+        return netto;
+    }
+
+    public void setNetto(double netto) {
+        this.netto = netto;
+    }
+
+    public ProductBatchDTO() {
+    }
+
+    public ProductBatchDTO(int productBatchId, int recipeId, int status, int userId, int ingredientBatchId, double tara, double netto) {
+        this.productBatchId = productBatchId;
+        this.recipeId = recipeId;
+        this.status = status;
+        this.userId = userId;
+        this.ingredientBatchId = ingredientBatchId;
+        this.tara = tara;
+        this.netto = netto;
+    }
 }
