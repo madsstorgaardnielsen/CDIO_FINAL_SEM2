376f7ea6fe46e4999aa9826482c120fc1f5444f7
Mads Storgaard-Nielsen <madssn@hotmail.com>
2020-06-09 10:36:35 +0200

    Added logic to IngredientDTO, IngredientDAO, IngredientController

--- src/main/java/controllers/IngredientController.java
+++ src/main/java/controllers/IngredientController.java
@@ -1,4 +1,73 @@
 package controllers;
 
+import dao.IngredientDAO;
+import dao.UserDAO;
+import dto.IngredientDTO;
+import validation.InputValidation;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class IngredientController {
+    private static IngredientController instance;
+
+    static {
+        try {
+            instance = new IngredientController();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    private final InputValidation validation;
+    private final IngredientDAO ingredientDAO;
+    private IngredientDTO ingredientDTO;
+
+    private IngredientController() throws SQLException {
+        this.ingredientDAO = new IngredientDAO();
+        this.ingredientDTO = new IngredientDTO();
+        this.validation = new InputValidation();
+    }
+
+    public static IngredientController getInstance() {
+        return instance;
+    }
+
+    public void deleteIngredient(int id) {
+        try {
+            ingredientDAO.deleteIngredient(id);
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addIngredient(int id, String name, String supplier) {
+        ingredientDTO = new IngredientDTO(id, name, supplier);
+        try {
+            ingredientDAO.addIngredient(ingredientDTO);
+        } catch (SQLException | IOException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void updateIngredient(int id, String name, String supplier) {
+        ingredientDTO = new IngredientDTO(id, name, supplier);
+
+        try {
+            ingredientDAO.updateIngredient(ingredientDTO);
+        } catch (SQLException | IOException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public ArrayList<IngredientDTO> getAllIngredients() throws Exception {
+        return ingredientDAO.getAllIngredients();
+    }
+
+    public IngredientDTO getIngredient(int id) throws Exception {
+        return ingredientDAO.getIngredient(id);
+    }
+
+
 }
--- src/main/java/dao/IngredientDAO.java
+++ src/main/java/dao/IngredientDAO.java
@@ -1,4 +1,125 @@
 package dao;
 
+import db.DBConnection;
+import dto.IngredientDTO;
+import dto.UserDTO;
+
+import java.io.IOException;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class IngredientDAO {
+
+    private static IngredientDAO instance;
+
+    static {
+        try {
+            instance = new IngredientDAO();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    PreparedStatement statement;
+    private DBConnection database;
+
+    public IngredientDAO() throws SQLException {
+        database = new DBConnection();
+    }
+
+    public static IngredientDAO getInstance() {
+        return instance;
+    }
+
+    public void addIngredient(IngredientDTO ingredient) throws SQLException, IOException {
+
+        String addIngredient = "{call AddIngredient(?,?,?)}";
+        PreparedStatement statement = database.callableStatement(addIngredient);
+        statement.setInt(1, ingredient.getIngredientID());
+        statement.setString(2, ingredient.getIngredientName());
+        statement.setString(3, ingredient.getSupplier());
+
+        try {
+            statement.executeUpdate();
+            System.out.println("Ingredient successfully added to database");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IOException("Ingredient could no be added");
+        }
+    }
+
+    public void updateIngredient(IngredientDTO ingredient) throws IOException, SQLException {
+
+        String updateIngredient = "{call UpdateIngredient(?,?,?)}";
+        PreparedStatement statement = database.callableStatement(updateIngredient);
+
+        statement.setInt(1, ingredient.getIngredientID());
+        statement.setString(2, ingredient.getIngredientName());
+        statement.setString(3, ingredient.getSupplier());
+
+        try {
+            statement.executeUpdate();
+            System.out.println("Ingredient successfully updated");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IOException("Ingredient could no be updated");
+        }
+    }
+
+    public void deleteIngredient(int id) throws IOException, SQLException {
+
+        String deleteIngredient = "{call DeleteIngredient(+"+id+")}";
+        PreparedStatement statement = database.callableStatement(deleteIngredient);
+        statement.setInt(1, id);
+
+        try {
+            statement.executeUpdate();
+            System.out.println("Ingredient successfully deleted");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IOException("Ingredient could no be deleted");
+        }
+    }
+
+
+    public ArrayList<IngredientDTO> getAllIngredients() throws Exception {
+        ArrayList<IngredientDTO> ingredientList = new ArrayList<>();
+        CallableStatement stmt = database.callableStatement("{call GetAllIngredients}");
+        ResultSet rs = stmt.executeQuery();
+        IngredientDTO ingredientDTO;
+        try {
+            while (rs.next()) {
+                ingredientDTO = new IngredientDTO();
+                getIngredientInfo(rs, ingredientDTO);
+                ingredientList.add(ingredientDTO);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ingredientList;
+    }
+
+    private void getIngredientInfo(ResultSet rs, IngredientDTO ingredientDTO) throws SQLException {
+        ingredientDTO.setIngredientID(rs.getInt(1));
+        ingredientDTO.setIngredientName(rs.getString(2));
+        ingredientDTO.setIngredientSupplier(rs.getString(3));
+    }
+
+    public IngredientDTO getIngredient(int ID) throws Exception {
+        CallableStatement stmt = database.callableStatement("{call GetIngredient(?)}");
+        stmt.setInt(1, ID);
+        IngredientDTO ingredient = new IngredientDTO();
+        ResultSet resultSet = stmt.executeQuery();
+        try {
+            while (resultSet.next()) {
+                getIngredientInfo(resultSet, ingredient);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ingredient;
+    }
 }
