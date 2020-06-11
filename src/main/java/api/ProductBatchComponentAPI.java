7bdf7dbd8f88938a6ee2f06e93dd730442be379c
SanderJohansen <55443844+SanderJohansen@users.noreply.github.com>
2020-06-11 09:37:51 +0200

    Revert "Revert "Revert "Merge branch 'dev' of https://github.com/madsstorgaardnielsen/CDIO_FINAL_SEM2 into dev"""
    This reverts commit 8f3659091456be4934f4d6ca18d24f6b6b06da45.

--- src/main/java/api/ProductBatchComponentAPI.java
+++ src/main/java/api/ProductBatchComponentAPI.java
@@ -1,28 +1,4 @@
 package api;
 
-import dto.ProductBatchComponentDTO;
-import controllers.ProductBatchComponentController;
-
-import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
-
-@Path("productBatchComponent")
 public class ProductBatchComponentAPI {
-
-    @Path("/{ID}/")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getProductBatchComponent(@PathParam("ID") int Id) {
-        return Response.ok().build(); //ProductBatchComponentController.getInstance().getProductBatchComponent(Id);
-    }
-
-    @Path("/Tara/{ID}/{tara}/")
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response setTara(@PathParam("ID") int Id, @PathParam("tara") int tara) {
-        return Response.ok().build(); //ProductBatchComponentController.getInstance().setProductBatchComponentTara(ID, tara)
-    }
 }
