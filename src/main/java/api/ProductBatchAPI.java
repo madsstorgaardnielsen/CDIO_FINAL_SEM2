8f3659091456be4934f4d6ca18d24f6b6b06da45
SanderJohansen <55443844+SanderJohansen@users.noreply.github.com>
2020-06-11 09:34:50 +0200

    Revert "Revert "Merge branch 'dev' of https://github.com/madsstorgaardnielsen/CDIO_FINAL_SEM2 into dev""
    This reverts commit 13880e717a9983d58945277fa2f6da16c47f1432.

--- src/main/java/api/ProductBatchAPI.java
+++ src/main/java/api/ProductBatchAPI.java
@@ -1,4 +1,34 @@
 package api;
 
+import dto.ProductBatchDTO;
+import controllers.ProductBatchController;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+@Path("productBatch")
 public class ProductBatchAPI {
+    @Path("/afvejning/{batchID}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getBatch(@PathParam("batchID") int batchId){
+        return Response.ok().build(); ////ProductBatchController.getInstance().getProductBatch(batchId)
+    }
+
+    @Path("/afvejning/{batchID}/")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getRecipeName(@PathParam("batchID") int batchId) {
+        return Response.ok(batchId).build(); //ProductBatchController.getInstance().getRecipeName(batchId);
+    }
+
+    @Path("/afvejning/{batchID}/")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response setStatus(@PathParam("batchID") int batchId) {
+        return Response.ok().build(); //ProductBatchController.getInstance().setStatus();
+    }
 }
